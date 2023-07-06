2a140f688a393e82578d44196f9eb9c8ff51d46e
Junghyun Nam <13118114+cr0sh@users.noreply.github.com>
2023-07-06 23:52:37 +0900

    feat: identity implementations

--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,30 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
+/// A single **guest-side** paramter which can be moved across guest-host boundary.
+pub trait Parameter {
+    type Host;
+    fn from_host(host: Self::Host) -> Self;
+    fn into_host(self) -> Self::Host;
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+macro_rules! identity_impls {
+    () => {};
+    ($type:ty $(,$other:ty)* $(,)?) => {
+        identity_impls!(@ $type);
+        identity_impls!($($other,)*);
+    };
+    (@ $type:ty) => {
+        impl $crate::Parameter for $type {
+            type Host = $type;
+            fn from_host(host: Self::Host) -> Self {
+                host
+            }
+            fn into_host(self) -> Self::Host {
+                self
+            }
+        }
+    };
+}
+
+identity_impls!(u8, u16, u32, u64, i8, i16, i32, i64);
 
     #[test]
     fn it_works() {
