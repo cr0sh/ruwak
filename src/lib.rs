d045566c9e444e297f46c0a2aaba3857d3e97a93
Junghyun Nam <13118114+cr0sh@users.noreply.github.com>
2023-07-07 00:15:41 +0900

    feat: add function trait and its impl generator (#2)

--- src/lib.rs
+++ src/lib.rs
@@ -26,9 +26,32 @@ macro_rules! identity_impls {
 
 identity_impls!(u8, u16, u32, u64, i8, i16, i32, i64);
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
+/// A function definition that can be shared between a guest and a host.
+pub trait Function {}
+
+macro_rules! fn_impls {
+    () => {};
+    ($type:tt $(,$other:tt)* $(,)?) => {
+        fn_impls!(@ $type $(,$other)*);
+        fn_impls!($($other,)*);
+    };
+    (@ $head:tt) => {
+        impl<$head> $crate::Function for fn($head)
+        where
+            $head: $crate::Parameter
+        {
+        }
+    };
+    (@ $head:tt $(,$other:tt)*) => {
+        impl<$head $(,$other)*> $crate::Function for fn($head $(,$other)*)
+        where
+            $head: $crate::Parameter
+            $(,
+                $other: $crate::Parameter
+            )*
+        {
+        }
+    };
 }
+
+fn_impls!(T1, T2, T3, T4, T5, T6, T7, T8);
