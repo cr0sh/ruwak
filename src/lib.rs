7269c1752264fe38d3194d136e23910262fc5a1b
Junghyun Nam <13118114+cr0sh@users.noreply.github.com>
2023-07-08 16:44:30 +0900

    feat: Add GuestStringView, GuestMemoryView

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,5 @@
+use wasmtime::{Memory, Store};
+
 /// A single **guest-side** paramter which can be moved across guest-host boundary.
 pub trait Parameter {
     type Host;
@@ -50,6 +52,95 @@ identity_impls!(
     *mut i32,
     *mut i64,
 );
+
+/// A FFI-safe `&str` equivalent passed from guest side.
+#[repr(C)]
+#[derive(Clone, Copy)]
+pub struct GuestStringView {
+    len: u32,
+    ptr: u32,
+}
+
+impl GuestStringView {
+    /// Creates a new [`GuestStringView`] instance.
+    pub fn new(s: &str) -> Self {
+        Self {
+            len: s.len().try_into().expect("len overflow"),
+            ptr: (s.as_ptr() as usize).try_into().expect("ptr overflow"),
+        }
+    }
+
+    /// Constructs a `&str`, a reference of string slice from this view.
+    pub fn as_slice<T>(self, memory: Memory, store: &Store<T>) -> &str {
+        // SAFETY: this is safe because the wasmtime API asserts the `store` owns the `memory` and
+        // the lifetime of the slice reference is bounded by the store's lifetime.
+        unsafe {
+            std::str::from_utf8_unchecked(
+                memory
+                    .data(store)
+                    .get(
+                        usize::try_from(self.ptr).expect("range from overflow")
+                            ..usize::try_from(self.ptr + self.len).expect("range to overflow"),
+                    )
+                    .expect("memory range out of bounds"),
+            )
+        }
+    }
+}
+
+impl<'a> Parameter for &'a str {
+    type Host = GuestStringView;
+
+    fn from_host(_host: Self::Host) -> Self {
+        unreachable!("host created an instance of GuestStringView")
+    }
+
+    fn into_host(self) -> Self::Host {
+        GuestStringView::new(self)
+    }
+}
+
+/// A FFI-safe `&[u8]` equivalent passed from guest side.
+#[repr(C)]
+#[derive(Clone, Copy)]
+pub struct GuestMemoryView {
+    len: u32,
+    ptr: u32,
+}
+
+impl GuestMemoryView {
+    /// Creates a new [`GuestMemoryView`] instance.
+    pub fn new(s: &[u8]) -> Self {
+        Self {
+            len: s.len().try_into().expect("len overflow"),
+            ptr: (s.as_ptr() as usize).try_into().expect("ptr overflow"),
+        }
+    }
+
+    /// Constructs a `&[u8]`, a reference of u8 slice from this view.
+    pub fn as_str<T>(self, memory: Memory, store: &Store<T>) -> &[u8] {
+        // safety:
+        memory
+            .data(store)
+            .get(
+                usize::try_from(self.ptr).expect("range from overflow")
+                    ..usize::try_from(self.ptr + self.len).expect("range to overflow"),
+            )
+            .expect("memory range out of bounds")
+    }
+}
+
+impl<'a> Parameter for &'a [u8] {
+    type Host = GuestMemoryView;
+
+    fn from_host(_host: Self::Host) -> Self {
+        unreachable!("host created an instance of GuestMemoryView")
+    }
+
+    fn into_host(self) -> Self::Host {
+        GuestMemoryView::new(self)
+    }
+}
 
 /// A function definition that can be shared between a guest and a host.
 pub trait Function {}
